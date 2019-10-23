8f1895b0cdf6304b12b635b8004713716319d318
Ashwin Vivek <ashwin9798@gmail.com>
2019-10-23 16:55:34 -0700

    use devise to create rough login/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
   get 'static_pages/about'
   get 'static_pages/help'
   get 'static_pages/home'
+
+  devise_scope :user do
+    match '/login' => "devise/sessions#new", :as => :login, :via => [:get, :post]
+    match '/signup' => "devise/registrations#new", :as => :signup, :via => [:get, :post]
+  end
 
 end

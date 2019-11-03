e3c1d18c9079220e0977c6990eecc0fd44ce702f
suyashSax <suyash2099@gmail.com>
2019-11-02 22:58:49 -0700

    Fix migration error on cold start

--- db/migrate/20191031052231_add_firstname_to_joiners.rb
+++
@@ -1,6 +0,0 @@
-class AddFirstnameToJoiners < ActiveRecord::Migration[5.1]
-  def change
-    add_column :joiners, :firstname, :string, null: false, default: ""
-    add_column :joiners, :lastname, :string, null: false, default: ""
-  end
-end

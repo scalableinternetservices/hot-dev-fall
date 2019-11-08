da128bcdb5340e599b0e681534da4b3f265e6f2d
suyashSax <suyash2099@gmail.com>
2019-11-07 20:07:13 -0800

    convert to join query schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191108031730) do
+ActiveRecord::Schema.define(version: 20191108034014) do
 
   create_table "contracts", force: :cascade do |t|
     t.integer "sharer_id"
@@ -25,14 +25,10 @@ ActiveRecord::Schema.define(version: 20191108031730) do
   end
 
   create_table "joiners", force: :cascade do |t|
-    t.string "firstname", default: "", null: false
-    t.string "lastname", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "matched", default: "", null: false
-    t.string "host_email", default: "", null: false
-    t.index ["email"], name: "index_joiners_on_email", unique: true
+    t.integer "user_id"
+    t.string "service"
+    t.string "status"
+    t.index ["user_id"], name: "index_joiners_on_user_id"
   end
 
   create_table "sharers", force: :cascade do |t|

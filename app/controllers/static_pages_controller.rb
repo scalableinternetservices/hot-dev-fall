9745772f6bf6caf1d95a63e62bf7df80d7f35827
Ashwin Vivek <ashwin9798@gmail.com>
2019-11-12 19:30:40 -0800

    show more information on cards

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,8 @@ class StaticPagesController < ApplicationController
   def help
   end
 
+  ContractSharedObject = Struct.new(:service, :user_email, :user_id, :joiners_array)
+  ContractJoinedObject = Struct.new(:service, :owner_email, :account_username, :account_password)
   def home
       if user_signed_in?
           sharer = Sharer.find_by(params[:id])
@@ -16,20 +18,27 @@ class StaticPagesController < ApplicationController
           else
               @my_contracts_shared = []
               Contract.where(sharer_uid: @user.id).find_each do |contract|
-                @my_contracts_shared.push(contract)
-                Sharer.where(user_id:contract.sharer_uid).find_each do |contract_creator|
-                    @service = contract_creator.service
-                    @account_name = contract_creator.account_id
+                contractObject = ContractSharedObject.new
+                contractObject.service = "Netflix"
+                contractObject.joiners_array = []
+                Joiner.where(user_id:contract.joiner_uid).find_each do |contract_joiner|
+                    user = User.find(contract_joiner.user_id)
+                    contractObject.joiners_array.push(user)
                 end
+                @my_contracts_shared.push(contractObject)
               end
 
               @my_contracts_joined = []
               Contract.where(joiner_uid: @user.id).find_each do |contract|
-                @my_contracts_joined.push(contract)
+                contractObject = ContractJoinedObject.new
+                contractObject.service = "Netflix"
                 Sharer.where(user_id:contract.sharer_uid).find_each do |contract_creator|
-                    @service = contract_creator.service
-                    @account_name = contract_creator.account_id
+                    user = User.find(contract_creator.user_id)
+                    contractObject.owner_email = user.email
+                    contractObject.account_username = contract.account_id
+                    contractObject.account_password = contract.account_password
                 end
+                @my_contracts_joined.push(contractObject)
               end
 
               @my_share_requests = []

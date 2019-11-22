8691073be2c7c1d0dc3c1cd59e8c18335b1e3804
Edgar Lopez <elopez42@g.ucla.edu>
2019-11-22 04:43:12 +0000

    add simple seeding script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  for x in 1..100
+    Joiner.create(user_id: x, service: "Netflix", status: "Pending")
+  end
+
+  for x in 101..200
+    Sharer.create(user_id: x, service: "Netflix", size: 5, account_id:"helpme@me.com", account_password:"MOUSE", status: "Pending", plan_cost: 10)
+  end
+

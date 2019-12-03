f8037470fa1d57f76f862646c9265be3c249cf2f
Ashwin Vivek <ashwin9798@gmail.com>
2019-12-02 17:04:25 -0800

    remove did finish signup variable

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,73 +8,66 @@ class StaticPagesController < ApplicationController
   ContractSharedObject = Struct.new(:id, :service, :user_email, :user_id, :joiners_array, :price, :account_username)
   ContractJoinedObject = Struct.new(:id, :service, :owner_email, :account_username, :account_password, :price)
   ContractJoinerObject = Struct.new(:id, :user)
-  
+
   def home
       if user_signed_in?
-          sharer = Sharer.find_by(params[:id])
-          joiner = Joiner.find_by(params[:id])
           @user = current_user
+          @my_contracts_shared = []
 
-          if sharer.nil? && joiner.nil?
-              @need_further_setup = true
-          else
-              @my_contracts_shared = []
+          processed = Set.new
 
-              processed = Set.new
+          Contract.where(sharer_uid: @user.id).find_each do |contract|
+            if processed.include?(contract.sharer_id)
+              next
+            end
 
-              Contract.where(sharer_uid: @user.id).find_each do |contract|
-                if processed.include?(contract.sharer_id)
-                  next
-                end
+            cache_key = cache_key_share_contract(contract)
+            puts "KEY: #{cache_key}"
 
-                cache_key = cache_key_share_contract(contract)
-                puts "KEY: #{cache_key}"
-                
-                # TODO: Figure out when to expire i.e. length of a session/load test
-                contractObject = Rails.cache.fetch(cache_key, expires_in: 5.minutes) do
-                  process_share_contract(contract)
-                end
-        
-                # puts "OBJ #{contractObject}"
-                processed.add(contract.sharer_id)
-                @my_contracts_shared.push(contractObject)
-              end
+            # TODO: Figure out when to expire i.e. length of a session/load test
+            contractObject = Rails.cache.fetch(cache_key, expires_in: 5.minutes) do
+              process_share_contract(contract)
+            end
 
-              @my_contracts_joined = []
-              Contract.where(joiner_uid: @user.id).find_each do |contract|
-                contractObject = ContractJoinedObject.new
-
-                contractObject.id = contract.id
-                contractObject.account_username = contract.account_id
-                contractObject.account_password = contract.account_password
-                contractObject.price = contract.price
-
-                # TODO: Add service to joins model, avoid JOIN
-                shareRequest = Sharer.find(contract.sharer_id)
-                contractObject.service = shareRequest.service
-
-                creator = User.find(shareRequest.user_id)
-                contractObject.owner_email = creator.email
-
-                @my_contracts_joined.push(contractObject)
-              end
-
-              @my_share_requests = []
-              Sharer.where(user_id: @user.id).where(status: "Pending").find_each do |share|
-                @my_share_requests.push(share)
-              end
-
-              @my_join_requests = []
-              Joiner.where(user_id: @user.id).where(status: "Pending").find_each do |join|
-                @my_join_requests.push(join)
-              end
-
-              # puts @user.id
-              # puts @my_contracts_shared
-              # puts @my_contracts_joined
-              # puts @my_share_requests
-              # puts @my_join_requests
+            # puts "OBJ #{contractObject}"
+            processed.add(contract.sharer_id)
+            @my_contracts_shared.push(contractObject)
           end
+
+          @my_contracts_joined = []
+          Contract.where(joiner_uid: @user.id).find_each do |contract|
+            contractObject = ContractJoinedObject.new
+
+            contractObject.id = contract.id
+            contractObject.account_username = contract.account_id
+            contractObject.account_password = contract.account_password
+            contractObject.price = contract.price
+
+            # TODO: Add service to joins model, avoid JOIN
+            shareRequest = Sharer.find(contract.sharer_id)
+            contractObject.service = shareRequest.service
+
+            creator = User.find(shareRequest.user_id)
+            contractObject.owner_email = creator.email
+
+            @my_contracts_joined.push(contractObject)
+          end
+
+          @my_share_requests = []
+          Sharer.where(user_id: @user.id).where(status: "Pending").find_each do |share|
+            @my_share_requests.push(share)
+          end
+
+          @my_join_requests = []
+          Joiner.where(user_id: @user.id).where(status: "Pending").find_each do |join|
+            @my_join_requests.push(join)
+          end
+
+          # puts @user.id
+          # puts @my_contracts_shared
+          # puts @my_contracts_joined
+          # puts @my_share_requests
+          # puts @my_join_requests
       else
       end
 

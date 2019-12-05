b0ef04468209f93473b9d7f8cd0686b4b4b97fe3
suyashSax <suyash2099@gmail.com>
2019-12-04 16:08:09 -0800

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+n_users = 250
+
 def update_contract_cost(sharer)
     contracts = Contract.where(sharer_id: sharer.id)
     new_price = sharer.plan_cost/(contracts.length + 1)
@@ -11,14 +13,16 @@ def update_contract_cost(sharer)
     end
 end
   
-for user_id in 1..500
+for user_id in 1..n_users
     user = User.create! :email => "user#{user_id}@fakeemail.com", :password => 'topsecret', :password_confirmation => 'topsecret'
 end
+
+puts "CREATED: #{User.count} Users"
   
-for user_id in 1..500
-    for i in 1...rand(5...10)
+for user_id in 1..n_users
+    for i in 1...rand(0...50)
         r = rand(10)
-        if r < 7
+        if r <= 6
             #   Joiner.create(user_id: user_id, service: ["Netflix", "Disney+", "Hulu"].sample, status: "Pending")
             #   Joiner.create(user_id: user_id, service: ["Netflix", "Disney+", "Hulu"].sample, status: "Pending")
             joiner = Joiner.create(user_id: user_id, service: ["Netflix", "Disney+", "Hulu"].sample, status: "Pending")
@@ -67,20 +71,19 @@ end
 
 puts "CREATED:"
 puts "COUNT: #{Contract.count} Contracts"
-puts "COUNT: #{User.count} Users"
 puts "COUNT: #{Sharer.count} Share Reqs"
 puts "COUNT: #{Joiner.count} Join Reqs"
 
 puts "COUNT: #{Joiner.where("status == ?", "Pending").count} PENDING JOIN REQS"
-puts "COUNT: #{Joiner.where("status == ?", "Pending").count} PENDING SHARE REQS"
+puts "COUNT: #{Sharer.where("status == ?", "Pending").count} PENDING SHARE REQS"
 
 puts "COUNT: #{Joiner.where("status == ?", "Complete").count} COMPLETE JOIN REQS"
-puts "COUNT: #{Joiner.where("status == ?", "Complete").count} COMPLETE SHARE REQS"
+puts "COUNT: #{Sharer.where("status == ?", "Complete").count} COMPLETE SHARE REQS"
 
 puts "FINISHED HOME PAGE SEEDING STUFF"
   
 Contract.all.each do |c|
-    for i in 1...rand(1...20)
+    for i in 1...rand(0...25)
         Message.create(content: "Hi, what's the new password?", contract_id: c.id, sender_email: "fake@email.com")
     end
 end
